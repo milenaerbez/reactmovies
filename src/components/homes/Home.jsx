bdf397d612716cd1ee563e00ae4e5b978ba07fbb
Milena Erbez <erbez01@gmail.com>
2023-01-10 17:46:30 +0100

    dodaci

--- src/components/homes/Home.jsx
+++ src/components/homes/Home.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import { HomeCard } from './HomeCard'
-
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick"
 
 export const Home = ({items}) => {
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+       
+      };
   return (
     <>
     <div className='homeContainer'>
+    <Slider {...settings}>
     {items.map((item) => (
   <HomeCard key={item.id} item={item} />
 ))}
+</Slider>
     </div>
     
     </>
